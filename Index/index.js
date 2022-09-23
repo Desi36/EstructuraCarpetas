710bcbdad9769df5b6d328bf00b9876151fb9f7a
Desi36 <98879354+Desi36@users.noreply.github.com>
2022-09-23 10:57:03 +0200

    Create index.js

---
+++ Index/index.js
@@ -0,0 +1,8 @@
+function Persona(name, age, sex) {
+  this.name = name;
+  this.age = age;
+}
+   var desi = new Persona('Desi Diaz', 23);
+
+   alert('Hola, mi nombre es '+desi.name);
+   alert('Hola, mi edad  es '+desi.age);
